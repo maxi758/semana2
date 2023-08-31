63ea839629215fe7e92d7f5701a0bfe4d478b1d1
maxi758 <maxi758@gmail.com>
2023-08-31 01:22:35 -0300

    Agregar archivos de proyecto.

---
+++ superheroes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace superheroes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Superheroe superheroe1 = new Superheroe("max", 40, 50, 60);
+            Superheroe superheroe2 = new Superheroe("jose", 40, 50, 60);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ superheroes/Superheroe.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace superheroes
+{
+    internal class Superheroe
+    {
+        private string nombre;
+        private int fuerza;
+        private int resistencia;
+        private int superpoderes;
+
+        public Superheroe(string nombre, int fuerza, int superpoderes, int resistencia)
+        {
+            Nombre = nombre;
+            Fuerza = fuerza;
+            Superpoderes = superpoderes;
+            Resistencia = resistencia;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Fuerza
+        {
+            get => fuerza;
+            private set
+            {
+                fuerza = ValidarNumero(value);
+            }
+        }
+
+        public int Superpoderes
+        {
+            get => superpoderes; set => superpoderes = value;
+        }
+        public int Resistencia
+        {
+            get => resistencia;
+            private set 
+            { 
+                resistencia = ValidarNumero(value); 
+            }
+        }
+        private int ValidarNumero(int value)
+        {//se pueden poner las validaciones en otro orden
+            if (value >= 0 && value <= 100)
+                return value;
+            else if (value < 0)
+                return 0;
+            else
+                return 100;
+        }
+
+
+    }
+}
