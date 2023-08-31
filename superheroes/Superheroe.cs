ce2c7aa7062ea97a0c474eca276370048cb5cb6b
maxi758 <maxi758@gmail.com>
2023-08-31 01:40:55 -0300

    creando metodos de clase

--- superheroes/Superheroe.cs
+++ superheroes/Superheroe.cs
@@ -31,7 +31,11 @@ namespace superheroes
 
         public int Superpoderes
         {
-            get => superpoderes; set => superpoderes = value;
+            get => superpoderes;
+            private set
+            {
+                superpoderes = ValidarNumero(value);
+            }
         }
         public int Resistencia
         {
@@ -50,7 +54,35 @@ namespace superheroes
             else
                 return 100;
         }
+        public string Ganador(Superheroe otro)
+        {
+            int puntos = 0;
+            if (Fuerza > otro.Fuerza)
+                puntos++;
+            else if (Fuerza < otro.Fuerza)
+                puntos--;
+            if (Superpoderes > otro.Superpoderes)
+                puntos++;
+            else if (Superpoderes < otro.Superpoderes)
+                puntos--;
+            if (Resistencia > otro.Resistencia)
+                puntos++;
+            else if (Resistencia < otro.Resistencia)
+                puntos--;
+            if (puntos > 0)
+                return $"{Nombre} es el ganador";
+            else if (puntos < 0)
+                return $"{otro.Nombre} es el ganador";
+            else
+                return "Empate";
+        }
 
+
+        public override string ToString()
+        {
+
+            return $"Nombre: {Nombre} Fuerza: {Fuerza} Superpoderes: {Superpoderes} Resistencia: {Resistencia}";
+        }
 
     }
 }
